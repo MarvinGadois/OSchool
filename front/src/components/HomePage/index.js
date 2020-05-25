4bb645895f82c8a78279a564d3719c5ed240c308
SamyC-Dev <cosamy2@gmail.com>
2020-05-25 14:55:11 +0200

    homepage begin

---
+++ front/src/components/HomePage/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+// import style
+import './style.css';
+
+
+const HomePage = () => {
+    return (
+        <div className="homePage">
+            coucou
+        </div>
+    );
+};
+
+export default HomePage;
