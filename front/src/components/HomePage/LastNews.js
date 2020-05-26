89d4467d6c4e8fe7aee6492d0e2893c8e50163e8
SamyC-Dev <cosamy2@gmail.com>
2020-05-26 17:09:46 +0200

    WIP:add map on lastnews

--- front/src/components/HomePage/LastNews.js
+++ front/src/components/HomePage/LastNews.js
@@ -4,33 +4,42 @@ import React from 'react';
 import './style.css';
 
 const LastNews = () => {
+
+    const news = lastNews.map((newOne) => (
+        <div key={newOne.id} className="one_news_card">
+            <h3>{newOne.titre}</h3>
+            <img className="img_one_news" src={newOne.image}></img>
+        </div>
+    ))
+
     return (
         <div className="last_news">
             <h2>Dernières actualités</h2>
-            <div className="one_news_card">
-                <h3>titre news</h3>
-                <img className="img_one_news" src={image_news}></img>
-            </div>
-            <div className="one_news_card">
-                <h3>titre news</h3>
-                <img className="img_one_news" src={image_news}></img>
-            </div>
-            <div className="one_news_card">
-                <h3>titre news</h3>
-                <img className="img_one_news" src={image_news}></img>
-            </div>
-            <div className="one_news_card">
-                <h3>titre news</h3>
-                <img className="img_one_news" src={image_news}></img>
-            </div>
-            <div className="one_news_card">
-                <h3>titre news</h3>
-                <img className="img_one_news" src={image_news}></img>
-            </div>
+            {news}
         </div>
-    );
+    )
 };
 
-const image_news = "https://www.worldloppet.com/wp-content/uploads/2018/10/no-img-placeholder.png"
+
+
+// FAKE DONNEE
+const lastNews = [
+    {
+        id: 1, titre: "Titre news", image: 'https://www.worldloppet.com/wp-content/uploads/2018/10/no-img-placeholder.png',
+    },
+    {
+        id: 2, titre: "Titre news", image: 'https://www.worldloppet.com/wp-content/uploads/2018/10/no-img-placeholder.png',
+    },
+    {
+        id: 3, titre: "Titre news", image: 'https://www.worldloppet.com/wp-content/uploads/2018/10/no-img-placeholder.png',
+    },
+    {
+        id: 4, titre: "Titre news", image: 'https://www.worldloppet.com/wp-content/uploads/2018/10/no-img-placeholder.png',
+    },
+    {
+        id: 5, titre: "Titre news", image: 'https://www.worldloppet.com/wp-content/uploads/2018/10/no-img-placeholder.png',
+    },
+
+];
 
 export default LastNews;
