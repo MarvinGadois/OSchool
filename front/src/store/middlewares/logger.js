345123f7c245abd5e6209b3fd166acceb2d76ace
SamyC-Dev <cosamy2@gmail.com>
2020-05-27 16:20:51 +0200

    add console.log in middleware logger

--- front/src/store/middlewares/logger.js
+++ front/src/store/middlewares/logger.js
@@ -1,6 +1,7 @@
 export default (store) => (next) => (action) => {
     switch (action.type) {
         default: {
+            console.log("MW-LOGGER")
             console.log('ACTION:', action.type);
             next(action);
         }
