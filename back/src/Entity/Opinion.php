52da69909d4ffcd13a42db4930a22ef4784b1e30
blupblupdoux <sarah.melin19403@gmail.com>
2020-05-27 11:12:03 +0200

    modiv .env et  .env.local

---
+++ back/src/Entity/Opinion.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\OpinionRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=OpinionRepository::class)
+ */
+class Opinion
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+}
