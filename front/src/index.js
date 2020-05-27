fc2cbc9022439f16005cdd8c35adbbea2db0585d
SamyC-Dev <cosamy2@gmail.com>
2020-05-27 16:55:02 +0200

    add react-router ok with switch 404

--- front/src/index.js
+++ front/src/index.js
@@ -4,6 +4,7 @@
 import React from 'react'; // couche 1
 import { render } from 'react-dom'; // couche 2
 import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 // == Import : local
 // Composants
@@ -13,9 +14,11 @@ import store from 'src/store';
 // 1. Élément React racine (celui qui contient l'ensemble de l'app)
 //    => crée une structure d'objets imbriqués (DOM virtuel)
 const rootReactElement = (
-    <Provider store={store}>
-        <App />
-    </Provider>
+    <Router>
+        <Provider store={store}>
+            <App />
+        </Provider>
+    </Router>
 );
 // 2. La cible du DOM (là où la structure doit prendre vie dans le DOM)
 const target = document.getElementById('root');
