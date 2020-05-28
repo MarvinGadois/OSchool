14acf23009b81d70fac1c9c058fea1b008b93a3f
Acoisu <aym.delafaire@gmail.com>
2020-05-28 17:16:00 +0200

    commit of the evening not a lot more than earlier

--- front/src/components/HomePage/Description.js
+++ front/src/components/HomePage/Description.js
@@ -11,32 +11,61 @@ import logo from "src/assets/O'school.png";
 const Description = () => {
   return (
     <div className="description">
-      <div className="container">
-        <h1>Bienvenue sur <img className="logo" src={logo} alt="logo O'school" /></h1>
+      <div className="container-head">
+        <div className="last-container-head">
+          <h1>Bienvenue sur </h1><img className="logo" src={logo} alt="logo O'school" />
 
-        <div className="description_main">
-          <p>O’school est une plateforme web facilitant les échanges en ligne entre
-            les différent parties de l’éducation.
-          </p>
+          <div className="description_main">
+            <p>O’school est une plateforme web facilitant les échanges en ligne entre
+              les différent parties de l’éducation.
+            </p>
+          </div>
         </div>
       </div>
 
-      <div>
-        <img className="icon" src={icon1} alt="icon inscription"/>
-        <p>Inscription effectuée par l’administration</p>
+      <div className="container-body">
+        <div className="catchphrase">
 
-      </div>
-      <div>
+          <div>
+            <img className="icon" src={icon1} alt="icon inscription"/>
+            <p>Inscription effectuée par l’administration</p>
 
-      <img className="icon" src={icon2} alt="icon inscription"/>
-        <p>Accès facile et en direct de tous les documents</p>
+          </div>
+          <div>
 
-      </div>
+            <img className="icon" src={icon2} alt="icon inscription"/>
+            <p>Accès facile et en direct de tous les documents</p>
 
-      <div>
+          </div>
 
-      <img className="icon" src={icon3} alt="icon inscription"/>
-        <p>Simple et intuitif</p>
+          <div>
+
+            <img className="icon" src={icon3} alt="icon inscription"/>
+            <p>Simple et intuitif</p>
+
+          </div>
+        </div>
+        <div className="paragraphs">
+
+          <div>
+            <h2>Administration</h2>
+            <p>Notre système de gestion de vie scolaire réduit le travail administratif des écoles, des enseignants, mais aussi des élèves et des parents. Notamment avec la centralisation de tous les documents liés à l’éducation.
+            </p>
+          </div>
+
+          <div>
+            <h2>Enseignants</h2>
+            <p>O’school permet la gestion des enseignants, nous fournissons une interface pour consulter les
+              emplois du temps, enregistrer les résultats des élèves et le cahier de correspondance.
+            </p>
+          </div>
+
+          <div>
+            <h2>Parents-Elèves</h2>
+            <p>Cette plateforme est une solution très appréciée par la communauté parentale. Nous assurons le suivi scolaire de leurs enfants tout en veillant au respect de la confidentialité de leurs données personnelles.</p>
+          </div>
+
+        </div>
 
       </div>
     </div>
