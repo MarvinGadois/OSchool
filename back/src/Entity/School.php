b39f8a7736140dfda20ebde9b152d9135c850a65
blupblupdoux <sarah.melin19403@gmail.com>
2020-06-03 12:51:55 +0200

    h

--- back/src/Entity/School.php
+++ back/src/Entity/School.php
@@ -6,6 +6,7 @@ use App\Repository\SchoolRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
 
 /**
  * @ORM\Entity(repositoryClass=SchoolRepository::class)
@@ -16,11 +17,13 @@ class School
      * @ORM\Id()
      * @ORM\GeneratedValue()
      * @ORM\Column(type="integer")
+     * @Groups({"school"})
      */
     private $id;
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Groups({"school"})
      */
     private $name;
 
