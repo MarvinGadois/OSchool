d33e61c3c9d8d26a5730e02c60d57295cf3bb5c6
Acoisu <aym.delafaire@gmail.com>
2020-06-05 18:17:41 +0200

    F-016-Page-Teacher-Classroom_implement_page

---
+++ front/src/components/Teacher_Classroom/index.js
@@ -0,0 +1,51 @@
+import React, { useEffect } from 'react';
+import { useSelector } from 'react-redux';
+
+//import imageFolders from '../../../../back/public/assets/images';
+
+
+import getSchoolNews from '../../utils/getSchoolNews';
+
+// import './styles.scss';
+
+
+const TeacherClassroom = () => {
+    const currentUser = useSelector((state) => state.user.user)
+    const currentClass = useSelector((state) =>state.user.currentClass) //we need it for the display of the entire page since it's about the class
+    // useEffect(() => {
+    //     getSchoolNews(currentUser.schools[0].id);
+    // }, [])
+
+    // const currentClass = currentUser.classrooms.map(classe => {
+    //     return (
+    //         <tr key={classe.id}>
+    //             <th scope="row">1</th>
+    //             <td>{classe.name}</td>
+    //             <td>28</td>
+    //             <td>{classe.level}</td>
+    //             <td>{currentUser.schools[0].name}</td>
+
+    //         </tr>
+    //     )
+    // })
+
+
+    console.log("salut c'est aymeric")
+
+    //return of the comosant to the page
+    return (
+      <div className="wrapper">
+          <div>
+
+            <p>{currentUser.schools[0].name}</p>
+
+            <p></p>
+
+          </div>
+
+      </div>
+    )
+    
+}
+
+export default TeacherClassroom;
