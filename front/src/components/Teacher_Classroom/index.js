9904d9462dcdc2f4c03d9bd38689cd505f22d1a0
Acoisu <aym.delafaire@gmail.com>
2020-06-06 20:24:27 +0200

    fix indetn and creation scss

--- front/src/components/Teacher_Classroom/index.js
+++ front/src/components/Teacher_Classroom/index.js
@@ -5,47 +5,106 @@ import { useSelector } from 'react-redux';
 
 
 import getSchoolNews from '../../utils/getSchoolNews';
-
-// import './styles.scss';
+import './style.scss';
 
 
 const TeacherClassroom = () => {
-    const currentUser = useSelector((state) => state.user.user)
-    const currentClass = useSelector((state) => state.user.currentClass) //we need it for the display of the entire page since it's about the class itself
-    // useEffect(() => {
-    //     getSchoolNews(currentUser.schools[0].id);
-    // }, [])
+  const currentUser = useSelector((state) => state.user.user)
+  const currentClass = useSelector((state) => state.user.currentClass) //we need it for the display of the entire page since it's about the class itself
+  // useEffect(() => {
+  //     getSchoolNews(currentUser.schools[0].id);
+  // }, [])
 
-    // const currentClass = currentUser.classrooms.map(classe => {
-    //     return (
-    //         <tr key={classe.id}>
-    //             <th scope="row">1</th>
-    //             <td>{classe.name}</td>
-    //             <td>28</td>
-    //             <td>{classe.level}</td>
-    //             <td>{currentUser.schools[0].name}</td>
+  // const currentClass = currentUser.classrooms.map(classe => {
+  //     return (
+  //         <tr key={classe.id}>
+  //             <th scope="row">1</th>
+  //             <td>{classe.name}</td>
+  //             <td>28</td>
+  //             <td>{classe.level}</td>
+  //             <td>{currentUser.schools[0].name}</td>
 
-    //         </tr>
-    //     )
-    // })
+  //         </tr>
+  //     )
+  // })
 
 
-    console.log("salut c'est aymeric")
+  console.log("salut c'est aymeric");
 
-    //return of the comosant to the page
-    return (
-      <div className="wrapper">
+  // return of the comosant to the page
+  return (
+    <div className="wrapper">
+      <div className="container-head">
+        <h2>{currentUser.schools[0].name}</h2>
+
+        <h2>Nombre d'élèves 20</h2>
+        <div className="container-head--placeholder">
+          <input type="text" className=""/>
+        </div>
+      </div>
+
+      <h1>Votre classe</h1>
+      <div className="container">
+        <div className="classrom-info">
+          <h2>Information de la classe</h2>
+          <ul>
+            <li>Nombre d'élèves</li>
+            <li>Lorem Ipsum</li>
+            <li>Lorem Ipsum</li>
+            <img src="" alt="Emploi du temps"/>
+          </ul>
+        </div>
+
+        <div className="membersList">
+          <h2>Liste des membres</h2>
+          <ul>
+            <li>
+              <p>student</p>
+              <p>moyenne</p>
+            </li>
+          </ul>
+        </div>
+
+        <div className="subject">
+          <h2>liste des matières</h2>
+          <ul>
+            <li>
+              <p>matière</p>
+              <p>professeur</p>
+            </li>
+          </ul>
+        </div>
+
+        <div className="lessons">
+          <h2>Vos cours/lessons</h2>
           <div>
-
-            <p>{currentUser.schools[0].name}</p>
-
-            <p></p>
+            <p>Nom de la matière</p>
+            <p>Nom du cours</p>
 
           </div>
+        </div>
 
+        <div className="homework">
+          <h2>Vos devoirs</h2>
+          <div>
+            <p>Nom du deovir</p>
+            <p>Nom de la matière</p>
+
+          </div>
+        </div>
+
+
+        <div className="correction">
+          <h2>Vos corrections</h2>
+          <div>
+            <p>Nom du devoir</p>
+            <p>Nom de la matière</p>
+
+          </div>
+        </div>
       </div>
-    )
-    
+    </div>
+  );
 }
 
 export default TeacherClassroom;
