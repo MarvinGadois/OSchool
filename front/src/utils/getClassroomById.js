a520540f8608807d33050547d2f7168922260cee
SamyC-Dev <cosamy2@gmail.com>
2020-06-06 16:30:56 +0200

    Add request get Classrooms by id ok

---
+++ front/src/utils/getClassroomById.js
@@ -0,0 +1,25 @@
+import axios from 'axios';
+import store from '../store/index';
+
+// Import base api url
+import { API_URL, GET_CLASS_BY_ID } from './constant';
+
+// Import action SET_OPINIONS
+import { setClassroom } from '../store/actions';
+
+
+const classByIdRequest = `${API_URL}${GET_CLASS_BY_ID}`;
+
+const getClassById = (idClass) => {
+
+    axios.get(classByIdRequest + idClass)
+        .then((res) => {
+            const Oneclass = res.data;
+            store.dispatch(setClassroom(Oneclass))
+        })
+        .catch((error) => {
+            console.trace(error);
+        });
+};
+
+export default getClassById;
