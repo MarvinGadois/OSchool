8cfac3826e600e57e1937e54383211651dee31fb
blupblupdoux <sarah.melin19403@gmail.com>
2020-06-12 14:42:31 +0200

    titre dynamique

--- front/src/components/Teacher_Classroom/index.js
+++ front/src/components/Teacher_Classroom/index.js
@@ -8,35 +8,46 @@ import getCurrentClassById from "../../utils/getCurrentClassroom";
 import "./style.scss";
 
 const TeacherClassroom = () => {
+
   let { id } = useParams();
+
+  // the current user connected
   const currentUser = useSelector((state) => state.user.user);
+
+  // the current class of the user connected
   const { currentClass } = useSelector((state) => state);
 
-  console.log('Ma classe courant:', currentClass)
-  // console.log('Ma classe courant:', currentClass.users)
+  console.log(currentClass);
+  // console.log(currentClass.school.name);
 
+  // get the lessons of a user
   const { lessons } = useSelector((state) => state);
   useEffect(() => {
     getLessons(currentUser.id);
   }, []);
 
+  // get the homework of a user
   const { homeworks } = useSelector((state) => state);
   useEffect(() => {
     getHomeworks(currentUser.id);
   }, []);
 
-  let allMembers;
-  let currentClassUserNb;
+ 
+  // get the students in the class 
+  
   useEffect(() => {
     getCurrentClassById(id);
   }, []);
 
+  // get the nb of students in the class 
+  let currentClassUserNb;
   currentClass.users
     ? (currentClassUserNb = currentClass.users.filter(
       (user) => user.roles[0] === "ROLE_STUDENT"
     ))
     : null;
 
+  // get the subjects of the current class
   let classroomSubject;
   if (currentClass.users) {
     console.log("ok");
@@ -59,6 +70,8 @@ const TeacherClassroom = () => {
     </div>
   ));
 
+  // get all members and their overall average
+  let allMembers;
   currentClass.users
     ? (allMembers = currentClass.users.map(
       (user) => (
@@ -71,6 +84,7 @@ const TeacherClassroom = () => {
       )))
     : null;
 
+  // get the last homework of the current user
   const allHomeworks = homeworks.map((homework) => (
     <div key={homework.id}>
       <p>{homework.title}</p>
@@ -83,31 +97,22 @@ const TeacherClassroom = () => {
   // return of the comosant to the page
   return (
     <div className="wrappered">
-      {/* <div className="container-of-head">
-        <h2>{currentUser.schools[0].name}</h2>
-        Nombre d'élèves:
-        {currentClass.users ? currentClassUserNb.length : null}
-        <div className="container-head--placeholder">
-          <input type="text" className="" />
-        </div>
-      </div> */}
       <div className="title-of-page">
-        <h1>Votre classe</h1>
+        <h1>Votre classe : {currentClass.name}</h1>
       </div>
       <div className="containered">
+
         <div className="classroomInfo">
           <h2>Information de la classe</h2>
-          <div className="bordered">
-            <ul>
-              <li>{currentUser.classrooms[0].name}</li>
-              <li>{currentUser.classrooms[0].level}</li>
-              <img
-                id="planning"
-                src="https://charivari.edumoov.com/wp-content/uploads/2018/03/Emploidu-temps-2.png"
-                alt="Emploie du temps"
-              ></img>
-            </ul>
-          </div>
+          <ul>
+            <li>{currentClass.name}</li>
+            <li>{currentClass.level}</li>
+          </ul>
+          <img
+              id="planning"
+              src="https://charivari.edumoov.com/wp-content/uploads/2018/03/Emploidu-temps-2.png"
+              alt="Emploi du temps"
+            ></img>
         </div>
 
         <div className="membersList">
@@ -147,6 +152,7 @@ const TeacherClassroom = () => {
             <p>Aucun devoir corrigé</p>
           </div>
         </div>
+
       </div>
     </div>
   );
